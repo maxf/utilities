a496138b51d6265a1f20916552847e7e13402c08
Max Froumentin <github@lapin-bl.eu>
2019-09-18 20:03:40 +0100

    place in subdirectory is easier and safer

--- rename-from-date.sh
+++ rename-from-date.sh
@@ -3,18 +3,30 @@
 # Read all the files in the current directory. If it's an image or a video that has a CreateDate,
 # Then rename it to have the date in the name. E.g. 2019:07:31-19:29.jpg
 
-for file in *; do
-    extension=`echo "${file##*.}" | tr '[:upper:]' '[:lower:]'`
-    createDate=`exiftool -CreateDate -d "%Y:%m:%d-%H:%M:%S" "$file" | cut -b 35-50`
-    if [ -n "$createDate" ]; then
-        newFilename=$createDate.$extension
-        if [ -f $newFilename ]; then
-            echo "$newFilename already exists. Not renaming $file."
+NEWDIR=renamed_files
+rm -rf $NEWDIR
+mkdir $NEWDIR
+
+for file in *.*; do
+    if [ -f "$file" ]; then
+        extension=`echo "${file##*.}" | tr '[:upper:]' '[:lower:]'`
+        createDate=`exiftool -CreateDate -d "%Y%m%d-%TZ" "$file" | cut -b 35-500`
+        if [ -n "$createDate" ]; then
+            newFilename=$createDate-00000.$extension
+            if [ -f $NEWDIR/$newFilename ]; then
+                x=1
+                extx=`printf "%05d\n" $x`
+                while [ -f $NEWDIR/$createDate-$extx.$extension ]; do
+                    x=$(( $x + 1 ))
+                    extx=`printf "%05d\n" $x`
+                done
+                newFilename=$createDate-$extx.$extension
+            fi
+            cp -v "$file" "$NEWDIR/$newFilename"
         else
-            echo "renaming $file to $newFilename"
-            mv $file $newFilename
+            echo "Can't find date info in $file. Not renaming."
         fi
     else
-        echo "Can't find date info in $file. Not renaming."
+        echo "\"$file\" is not a file. Skipping"
     fi
 done
